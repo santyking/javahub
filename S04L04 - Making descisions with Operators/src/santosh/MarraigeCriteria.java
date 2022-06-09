e765b15761537312a6bfb8dbcf1359f3381a0714
santyking <santosh.mhatre93@gmail.com>
2022-06-09 14:10:48 +0530

    Make Descisions with Operators using And or and other complex conditions.

---
+++ S04L04 - Making descisions with Operators/src/santosh/MarraigeCriteria.java
@@ -0,0 +1,19 @@
+package santosh;
+
+public class MarraigeCriteria
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int ageOfBoy  = 20;
+		int ageOfGirl = 18;
+		if ((ageOfBoy>=21)&&(ageOfGirl>=18)) {
+			System.out.println("Eligible for Marriage");
+		} else {
+			System.out.println("Not Eligible for Marraige");
+		}
+
+	}
+
+}
