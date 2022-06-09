9116fcb60130b5e9707e05e66f188b711ccd9160
santyking <santosh.mhatre93@gmail.com>
2022-06-09 12:32:04 +0530

    Basics of Java

---
+++ S03L06 - Primitive data types (Floating)/src/santosh/Floates.java
@@ -0,0 +1,16 @@
+package santosh;
+
+public class Floates
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		float fi = 10f/6f;
+		double di = 10d/6d;
+		System.out.println(fi);
+		System.out.println(di);
+//		as per best practices use double for better precision
+	}
+
+}
---
+++ S03L11 - Equations/src/santosh/Equations.java
@@ -0,0 +1,18 @@
+package santosh;
+
+public class Equations
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+//		(a+b)square = ((a*a)+2*a*b+(b*b));
+		int a = 25;
+		float b = 42.159f;
+		int result = (int)((a*a)+2*a*b+(b*b));
+		System.out.println(result);
+		
+				
+	}
+
+}
