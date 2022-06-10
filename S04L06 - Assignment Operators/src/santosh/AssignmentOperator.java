9a3eb56ccca9466ae5b3287c4d89170d9c4a929b
santyking <santosh.mhatre93@gmail.com>
2022-06-10 12:47:55 +0530

    Assignment Operators

---
+++ S04L06 - Assignment Operators/src/santosh/AssignmentOperator.java
@@ -0,0 +1,19 @@
+package santosh;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		System.out.println(x);
+		x+=5;
+		System.out.println(x);
+		x-=5;
+		System.out.println(x);
+		x*=5;
+		System.out.println(x);
+		x/=5;
+		System.out.println(x);
+	}
+
+}
