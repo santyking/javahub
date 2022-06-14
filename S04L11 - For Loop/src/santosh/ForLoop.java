be4cf8a98914a7d4c1e5d981e89db53b9c58e04f
santyking <santosh.mhatre93@gmail.com>
2022-06-14 13:35:35 +0530

    For Loop

---
+++ S04L11 - For Loop/src/santosh/ForLoop.java
@@ -0,0 +1,53 @@
+package santosh;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		type 1
+		/*
+		for (int i = 1; i < 5; i++) {
+			System.out.println(i);
+			
+		}
+		*/
+//		Type 2
+		/*
+		for (int i = 10; i >= 1; i--) {
+			System.out.println(i);
+		}
+		*/
+//		Type 3 
+//		Here we have used Declaration earlier
+		/*
+		int i = 1;
+		for (; i <= 5; i++) {
+			System.out.println(i);
+		}
+		*/
+//		Type 4
+//		Here we declare earlier and conditionalize later
+		/*
+		int i = 1;
+		for (;; i++) {
+			if (i>5) {
+				break;
+			}
+			System.out.println(i);
+		}
+		*/
+//		Type 5
+//		Here we do even the incrementation later
+		/*
+		int i = 1;
+		for (;;) {
+			if (i>5) {
+				break;
+			}
+			System.out.println(i);
+			i++;
+		}
+		*/
+	}
+
+}
