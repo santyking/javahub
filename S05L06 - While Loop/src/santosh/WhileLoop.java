0a68a4d76790e85624cfa91031db08bc35341e0e
santyking <santosh.mhatre93@gmail.com>
2022-06-14 17:30:13 +0530

    While Loop

---
+++ S05L06 - While Loop/src/santosh/WhileLoop.java
@@ -0,0 +1,25 @@
+package santosh;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		
+//		Normal While Loop
+		
+		while (i<=5) {
+			System.out.println(i);
+			i++;
+		}
+		
+		
+//		Infinite While loop
+		/*
+		while (true) {
+			System.out.println("Santosh");
+		}
+		*/
+	}
+
+}
