177ce9f1ae762b777f8632e3d93f585eecb1a761
santyking <santosh.mhatre93@gmail.com>
2022-06-17 18:23:13 +0530

    Method Overloading 1

---
+++ S06L03 - Method Overloading 1/src/santosh/MethodOverloading1.java
@@ -0,0 +1,12 @@
+package santosh;
+
+public class MethodOverloading1 {
+
+	public static void main(String[] args) {
+		System.out.println("The area is : "+area(10,12));
+	}	
+	
+	public static double area(double length,double breadth) {
+		return length*breadth;
+	}
+}
