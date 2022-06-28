4ed840e07099ec18488df9963d77327eda4a6ca4
santyking <santosh.mhatre93@gmail.com>
2022-06-28 13:02:51 +0530

    Classes 2

---
+++ S07L02 - Classes 2/src/com/santosh/CarBonet.java
@@ -0,0 +1,16 @@
+package com.santosh;
+
+public class CarBonet {
+	private String doors;
+	private String engine;
+	private String driver;
+	public int speed;
+	
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+}
---
+++ S07L02 - Classes 2/src/com/santosh/OperateCar.java
@@ -0,0 +1,11 @@
+package com.santosh;
+
+public class OperateCar {
+	
+	public static void main(String[] args) {
+		CarBonet car = new CarBonet();
+		car.setSpeed(10);	
+		System.out.println(car.getSpeed());
+	}
+	
+}
